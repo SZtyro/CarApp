21bbb8d646b85499bbb28e9cbd172d529a86fab3
SZtyro <fabiantrubic@gmail.com>
2024-12-08 09:41:25 +0100

    Libraries versions, config fixes

--- src/main/java/pl/sztyro/carapp/configuration/WebMVCConfig.java
+++ src/main/java/pl/sztyro/carapp/configuration/WebMVCConfig.java
@@ -1,6 +1,8 @@
 package pl.sztyro.carapp.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -8,10 +10,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 
 public class WebMVCConfig implements WebMvcConfigurer {
+
+    @Autowired
+    Environment env;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -22,9 +29,15 @@ public class WebMVCConfig implements WebMvcConfigurer {
                     @Override
                     protected Resource getResource(String resourcePath, Resource location) throws IOException {
                         Resource requestedResource = location.createRelative(resourcePath);
+                        List<String> list = Arrays.stream(env.getActiveProfiles()).filter("dev"::equals).toList();
+                        String path = null;
+                        if(list.size() == 1)
+                            path = "src/main/webapp/browser/index.html";
+                        else
+                            path = "webapps/ROOT/browser/index.html";
                         return requestedResource.exists() && requestedResource.isReadable()
                                 ? requestedResource
-                                : new FileSystemResource("webapps/ROOT/browser/index.html");
+                                : new FileSystemResource(path);
                     }
                 });
     }
